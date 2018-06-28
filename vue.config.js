6e7d508aa70c9e27be4f455ecf229d90345c99ba
linjiajian999 <724159997@qq.com>
2018-06-28 21:34:27 +0800

    update...

--- vue.config.js
+++ vue.config.js
@@ -15,8 +15,8 @@ module.exports = {
        // output as dist/index.html
        filename: 'index.html'
     },
-    // login: 'src/pages/login/index.ts',
-    // test: 'src/pages/test/index.ts'
+    login: 'src/pages/login/index.ts',
+    test: 'src/pages/test/index.ts'
   },
 
   runtimeCompiler: false,
